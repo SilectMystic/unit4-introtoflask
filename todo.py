d7b5c50a02b78ad7c1158b4e75cbefb1d2130fda
SilectMystic <cvasquez@brooklynsteamcenter.org>
2024-01-09 10:50:54 -0500

    yea done

--- todo.py
+++ todo.py
@@ -4,24 +4,35 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    connection = pymysql.connect(
+connection = pymysql.connect(
     database = 'cvasquez_todos',
     user = 'cvasquez',
     password = '242590909',
     host = '10.100.33.60',
     cursorclass = pymysql.cursors.DictCursor
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT `description` FROM `todos`")
-    results = cursor.fetchall()
+)
+
+@app.route('/', methods=['GET','POST'])
+def index():
     if request.method == 'POST':
+        cursor = connection.cursor()
         new_todo = request.form['new_todo']
-        cursor.execute(f"INSERT INTO `todos` (`description`) VALUES ({new_todo})")
+        cursor.execute(f"INSERT INTO `todos` (`description`) VALUES ('{new_todo}');")
+        cursor.close()
+        connection.commit()
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `todos`")
+    results = cursor.fetchall()
+    cursor.close()
+        
     return render_template('todos.html.jinja', todos_list=results)
 
-# @app.route('/delete_todo/<int:todo_index>', methods=['POST'])
-# def todo_delete(todo_index):
-#     del todos_list[todo_index]
-#     return redirect('/')
+
+@app.route('/delete_todo/<int:todo_index>', methods=['POST'])
+def todo_delete(todo_index):
+    cursor = connection.cursor()
+    cursor.execute(f"DELETE FROM `todos` WHERE `id` = {todo_index}")
+    cursor.close()
+    connection.commit()
+    return redirect('/')
